8033edea7af2b394c86bde321b224ae7b80385af
Alan Czemerinski <alanczeme@gmail.com>
2022-02-23 11:13:29 -0500

    commit tree d83781941b723e22642a5ee71e2941eadbe9b4fb

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_names)
+    array_names.map { |name| "Hello, my name is #{name}." }
+end
+
+def assign_rooms(speakers_list)
+    speakers_list.map.with_index { |name, i| "Hello, #{name}! You'll be assigned to room #{i+1}!" }
+end
+
+def printer(array_names)
+    batch_badge_creator(array_names).each { |badge| puts "#{badge}" }
+    assign_rooms(array_names).each { |assign| puts "#{assign}" }
+end
